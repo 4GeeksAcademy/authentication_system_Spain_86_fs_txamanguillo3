be1c6ca0be6301cc2e3bcd9eae9be82d9e7f9dc7
josemartinbm99 <josemartinbm99@gmail.com>
2025-02-20 16:26:51 +0000

    empiezo de stripe

---
+++ src/front/js/pages/protected/checkout.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+import { Link } from "react-router-dom";
+
+export const Checkout = () => {
+    const stripe = useStripe();
+    const elements = useElements();
+    const [clientSecret, setClientSecret] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        const paymentIntent = () => {
+            fetch('', {
+                method:['POST'],
+            })
+        }
+    }, [])
+
+    return(<>
+        <h1>checkout</h1>
+        <Link to="/session/payment">
+            <h1>Go to payment</h1>
+        </Link>
+    </>)
+}
