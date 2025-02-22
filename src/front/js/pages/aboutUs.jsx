8f8c01499bcab0e785698839bcef396cc43e13fb
Dorian Zuluaga <166706826+Dorianzuluaga@users.noreply.github.com>
2025-02-22 01:53:49 +0000

    Diseños finales Css y modificación del About-Us terminado!

--- src/front/js/pages/aboutUs.jsx
+++ src/front/js/pages/aboutUs.jsx
@@ -23,72 +23,68 @@ export const AboutUs = () => {
 
 
   return (isLoading) ? <Loader /> : (
-    <>
-    <div className="card-container">
-
-      <div className="title">
-        <h1 className="developer">DEVELOPER TEAM</h1>
-      </div>
-
-      <div className="Cards">
-        <div className="cardUs">
-          <img className="card-img" src={dorian} />
-          <div className="card-content">
-            <h3 className="card-title">Dorian Z.</h3>
-            <p>Software Developer</p>
-            <p className="card-description">
+    <div className="card-container-about-us">
+      <div className="card-About-Us">
+        <div className="title-about-us">
+          <h1 className="developer">DEVELOPER TEAM</h1>
+        </div>
+        <div className="card-Us">
+          <img className="card-img-about-us" src={dorian} />
+          <div className="card-content-about-us">
+            <h3 className="card-title-name-about-us">Dorian Z.</h3>
+            <p className="department">Software Developer</p>
+            <p className="card-description-about-us">
               "A veces desorganizado, a veces un desatre! Pero eso no me quita lo guapo"
             </p>
             <a href="https://linkedin.com/in/dorian-zuluaga-08132075">
-              <button className="card-button">About me</button>
+              <button className="card-button-about-me">About me</button>
             </a>
           </div>
         </div>
 
-        <div className="cardUs">
-          <img className="card-img" src={einar} />
-          <div className="card-content">
-            <h3 className="card-title">Einar F.</h3>
-            <p>Software Developer</p>
-            <p className="card-description">
+        <div className="card-Us">
+          <img className="card-img-about-us" src={einar} />
+          <div className="card-content-about-us">
+            <h3 className="card-title-name-about-us">Einar F.</h3>
+            <p className="department">Software Developer</p>
+            <p className="card-description-about-us">
               "A veces el rey de la concentración, y otras veces el emperador de la distracción"
             </p>
             <a href="https://www.linkedin.com/in/dorian-zuluaga-14873b251/">
-              <button className="card-button">About me</button>
+              <button className="card-button-about-me">About me</button>
             </a>
           </div>
         </div>
 
-        <div className="cardUs">
-          <img className="card-img" src={david} />
-          <div className="card-content">
-            <h3 className="card-title">David G.</h3>
-            <p>Software Developer</p>
-            <p className="card-description">
+        <div className="card-Us">
+          <img className="card-img-about-us" src={david} />
+          <div className="card-content-about-us">
+            <h3 className="card-title-name-about-us">David G.</h3>
+            <p className="department">Software Developer</p>
+            <p className="card-description-about-us">
               "A veces la persona más puntual del mundo, y otras veces el rey del "llego en 5 minutos!"
             </p>
             <a href="https://www.linkedin.com/in/dorian-zuluaga-14873b251/">
-              <button className="card-button">About me</button>
+              <button className="card-button-about-me">About me</button>
             </a>
           </div>
         </div>
 
-        <div className="cardUs">
-          <img className="card-img" src={jose} />
-          <div className="card-content">
-            <h3 className="card-title">Jose </h3>
-            <p>Software Developer</p>
-            <p className="card-description">
+        <div className="card-Us">
+          <img className="card-img-about-us" src={jose} />
+          <div className="card-content-about-us">
+            <h3 className="card-title-name-about-us">Jose </h3>
+            <p className="department">Software Developer</p>
+            <p className="card-description-about-us">
               "A veces soy un ninja de la programación, y otras veces un atleta de la procrastinación!"
             </p>
             <a href="https://www.linkedin.com/in/jose-martin-perez-29b188139">
-              <button className="card-button">About me</button>
+              <button className="card-button-about-me">About me</button>
             </a>
           </div>
         </div>
       </div>
+      <Footer className="footer-about-us" />
     </div>
-    <Footer />
-    </>
   );
 }
