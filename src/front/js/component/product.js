7890d29aa005798efe2469a3d84f1068b7245280
Dorian Zuluaga <166706826+Dorianzuluaga@users.noreply.github.com>
2025-02-23 02:36:21 +0000

    implementación de Css en productCard-product y retoque en about-us todo está terminado siguiendo la paleta de colores e igualdad en la funcionalidad de los botones tanto about-me como del carrito de compras.

--- src/front/js/component/product.js
+++ src/front/js/component/product.js
@@ -1,11 +1,12 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import { Context } from '../store/appContext';
+import "../../styles/product.css"
 
 export const Product = ({ product }) => {
 
-  const {store, actions} = useContext(Context);
+  const { store, actions } = useContext(Context);
 
-  const handleAgregarAlCarrito = ( ) => {
+  const handleAgregarAlCarrito = () => {
     if (product) {
       actions.agregarAlCarrito({
         id: product.id,
@@ -18,17 +19,17 @@ export const Product = ({ product }) => {
   }
 
 
-    return (<>
-        <div className="product" key={product.id}>
-            <div className="product-image-container">
-              <img src={product.imageId} />
-            </div>
-            <div className="product-card-body">
-              <h3 className="product-title">{product.title}</h3>
-              <p className="product-description">{product.description}</p>
-              <p className="product-price">{product.price}</p>
-            </div>
-            <button className='addTooCart' onClick={handleAgregarAlCarrito}><i class="fa-solid fa-cart-plus"></i></button>
-          </div>
-    </>)
+  return (<>
+    <div className="product" key={product.id}>
+      <div className="product-image-container">
+        <img src={product.imageId} />
+      </div>
+      <div className="product-card-body">
+        <h3 className="product-title">{product.title}</h3>
+        <p className="product-description">{product.description}</p>
+        <p className="product-price">{product.price} €</p>
+      </div>
+      <button className='addTooCart' onClick={handleAgregarAlCarrito}><i class="fa-solid fa-cart-plus"></i></button>
+    </div>
+  </>)
 }
