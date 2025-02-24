679cb634d83f75ca97c2a1c96387cc5edc39c754
Einar <einarescobarclavijo@gmail.com>
2025-02-24 09:59:03 +0000

    css poker

--- src/front/js/component/ProductCard.js
+++ src/front/js/component/ProductCard.js
@@ -13,11 +13,11 @@ export const ProductCards = () => {
 
   return (
     <div className="product-card-list">
-      {store.productList.map(product => {
-        return (
-          <Product product={product}/>
-        );
-      })}
-    </div>
+      {store.productList.map((product, index) => (
+      <div className="product-card" style={{ "--order": index + 1 }} key={product.id}>
+        <Product product={product} />
+      </div>
+  ))}
+</div>
   );
 };
