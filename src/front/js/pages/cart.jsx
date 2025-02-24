b37c45fe23bce5a4d0fa86912eafb3d79d369aa3
josemartinbm99 <josemartinbm99@gmail.com>
2025-02-24 17:04:00 +0000

    checkout y payment terminado

--- src/front/js/pages/cart.jsx
+++ src/front/js/pages/cart.jsx
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react'
+import React, { useContext } from 'react'
 import { Context } from '../store/appContext'
 import { Product } from '../component/product'
 import { Link } from 'react-router-dom'
