53e72805296ab25f39f1e1ada92474eca504fc02
Dorian Zuluaga <166706826+Dorianzuluaga@users.noreply.github.com>
2025-02-25 00:19:51 +0000

    he implementado el botón de busqueda y ahora es funcional! busca productos y devuelve toda la tienda al borrar todos los caracteres de busqueda

--- src/front/js/component/ProductCard.js
+++ src/front/js/component/ProductCard.js
@@ -9,15 +9,17 @@ export const ProductCards = () => {
   const { store, actions } = useContext(Context);
   useEffect(() => {
     actions.getProductList();
-  }, [])
+  }, []);
 
   return (
     <div className="product-card-list">
-      {store.productList.map(product => {
-        return (
-          <Product product={product}/>
-        );
-      })}
+      {store.filteredProducts.length > 0 ? (
+        store.filteredProducts.map((product) => (
+          <Product key={product.id} product={product} />
+        ))
+      ) : (
+        <p>No se encontraron productos</p>
+      )}
     </div>
   );
 };
